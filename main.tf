5065b0c7f7ad669a9df33fe56cf98e4b7fa37e3b
Mahesh Devulapelly <mdevulapelly@cog.int>
2025-12-08 21:38:42 -0800

    first commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-storage-example"
+  location = "East US"
+}
+
+module "storage_account" {
+  source = "./modules/storage-account"
+
+  storage_account_name     = "mystorageacct12345"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    Environment = "Development"
+    ManagedBy   = "Terraform"
+  }
+}
---
+++ modules/storage-account/variables.tf
@@ -0,0 +1,38 @@
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for the storage account"
+  type        = string
+}
+
+variable "account_tier" {
+  description = "Storage account tier"
+  type        = string
+  default     = "Standard"
+}
+
+variable "account_replication_type" {
+  description = "Storage account replication type"
+  type        = string
+  default     = "LRS"
+}
+
+variable "account_kind" {
+  description = "Storage account kind"
+  type        = string
+  default     = "StorageV2"
+}
+
+variable "tags" {
+  description = "Tags to apply to the storage account"
+  type        = map(string)
+  default     = {}
+}
